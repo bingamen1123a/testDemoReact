5c9997a5f923c3ee440923ef8f279c9459cb4ad9
Nguyen Hai <haikoi150423@gmail.com>
2025-03-03 23:33:28 +0700

    create Dad and Child file html

--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -2,50 +2,16 @@
 // function components
 
 import React from "react"; // IMPORT React
+import UserInfor from "./Userinfo";
 
 // thua ke lai Component cua react
 class MyComponents extends React.Component {
-
-    state = {
-        name: 'Nguyen Hai',
-        age: 22,
-        address: 'Hcm'
-    };
-    // neu khong chuyen ve arrow function se bao loi do js '() => {}'
-    handelClick = (event) => {
-        console.log(" You click me ")
-        console.log('My name is:', this.state.name)
-        console.log(event)
-        // cap nhat lai ten va tuoi tu state
-        this.setState({
-            name: 'Le Phu',
-            age: Math.floor((Math.random() * 100) + 1)
-        })
-
-    }
-
-    handleOnChaneInput = (event) => {
-        this.setState({
-            name: event.target.value
-        })
-    }
-
-    handleOnSubmit = (event) => {
-        event.preventDefault(); // nhan submit se khong load loai trang
-        console.log(this.state)
-    }
 
     // JSX  
     render() {
         return (
             <div>
-                My name is {this.state.name} and I'm {this.state.age} years old, I'm from {this.state.address}
-                <button onClick={(event) => { this.handelClick(event) }}>Click me</button>
-                <button onMouseOver={this.handelClick}> Hover click me</button>
-                <form onSubmit={(event) => { this.handleOnSubmit(event) }}>
-                    <input type="text" onChange={(event) => { this.handleOnChaneInput(event) }} />
-                    <button>Submit</button>
-                </form>
+                <UserInfor></UserInfor>
             </div >
         );
 
---
+++ src/components/Userinfo.js
@@ -0,0 +1,61 @@
+import React from "react";
+
+class UserInfor extends React.Component {
+
+    state = {
+        name: 'Nguyen Hai',
+        age: 22,
+        address: 'Hcm'
+    };
+    // neu khong chuyen ve arrow function se bao loi do js '() => {}'
+    handelClick = (event) => {
+        console.log(" You click me ")
+        console.log('My name is:', this.state.name)
+        console.log(event)
+        // cap nhat lai ten va tuoi tu state
+        this.setState({
+            name: 'Le Phu',
+            age: Math.floor((Math.random() * 100) + 1)
+        })
+
+    }
+
+    handleOnChaneInputName = (event) => {
+        // cap nhat lai ten tu state
+        this.setState({
+            name: event.target.value
+        })
+    }
+
+    handleOnChaneInputAge = (event) => {
+        // cap nhat lai tuoi tu state
+        this.setState({
+            age: event.target.value
+        })
+    }
+
+    handleOnSubmit = (event) => {
+        event.preventDefault(); // nhan submit se khong load loai trang
+        console.log(this.state)
+    }
+    render() {
+        return (
+            <div>
+                My name is {this.state.name} and I'm {this.state.age} years old, I'm from {this.state.address}
+                <button onClick={(event) => { this.handelClick(event) }}>Click me</button>
+                <button onMouseOver={this.handelClick}> Hover click me</button>
+                <form onSubmit={(event) => { this.handleOnSubmit(event) }}>
+                    <label>Your name:</label>
+                    <input value={this.state.name} type="text" onChange={(event) => { this.handleOnChaneInputName(event) }} />
+                    <button>Submit</button>
+
+                    <label>Your age:</label>
+                    <input value={this.state.age} type="text" onChange={(event) => { this.handleOnChaneInputAge(event) }} />
+                    <button>Submit</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default UserInfor;
