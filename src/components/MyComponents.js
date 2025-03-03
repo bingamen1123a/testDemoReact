c108562253ec676ddff08b63b49d6573a94d1e34
Nguyen Hai <haikoi150423@gmail.com>
2025-03-03 21:41:03 +0700

    state object

--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -5,11 +5,17 @@ import React from "react"; // IMPORT React
 
 // thua ke lai Component cua react
 class MyComponents extends React.Component {
+
+    state = {
+        name: 'Nguyen Phuoc Hai',
+        age: 22,
+        address: 'Hcm'
+    }
     // JSX
     render() {
         return (
-            <div> My First Compunents
-                {Math.random()}
+            <div>
+                My name is {this.state.name} and I am from {this.state.address}
             </div>
         );
 
