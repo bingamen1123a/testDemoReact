71a1c139e95b0f159a47cad3925b9342c686e013
Nguyen Hai <haikoi150423@gmail.com>
2025-03-09 17:35:49 +0700

    chuyen doi tat ca class component -> function component

--- src/components/AddUserinfo.js
+++ src/components/AddUserinfo.js
@@ -1,65 +1,113 @@
-import React from "react";
+import React, { useState } from "react";
 
-class AddUserInfor extends React.Component {
+// class AddUserInfor extends React.Component {
 
-    state = {
-        name: '',
-        age: '',
-        address: 'Hcm'
-    };
+//     state = {
+//         name: '',
+//         age: '',
+//         address: 'Hcm'
+//     };
+//     // neu khong chuyen ve arrow function se bao loi do js '() => {}'
+//     handelClick = (event) => {
+//         console.log(" You click me ")
+//         console.log('My name is:', this.state.name)
+//         console.log(event)
+//         // cap nhat lai ten va tuoi tu state
+//         this.setState({
+//             name: 'Le Phu',
+//             age: Math.floor((Math.random() * 100) + 1)
+//         })
+
+//     }
+
+//     handleOnChaneInputName = (event) => {
+//         // cap nhat lai ten tu state
+//         this.setState({
+//             name: event.target.value
+//         })
+//     }
+
+//     handleOnChaneInputAge = (event) => {
+//         // cap nhat lai tuoi tu state
+//         this.setState({
+//             age: event.target.value
+//         })
+//     }
+//     // lay du lieu tu input va truyen toi lop cha tu props handelAddNewUser
+//     handleOnSubmit = (event) => {
+//         event.preventDefault(); // nhan submit se khong load loai trang
+//         this.props.handelAddNewUser({
+//             id: Math.floor((Math.random() * 100) + 1) + ' random',
+//             name: this.state.name,
+//             age: this.state.age
+//         })
+//     }
+//     render() {
+//         return (
+//             <>
+//                 My name is {this.state.name} and I'm {this.state.age} years old, I'm from {this.state.address}
+//                 <button onClick={(event) => { this.handelClick(event) }}>Click me</button>
+//                 <button onMouseOver={this.handelClick}> Hover click me</button>
+//                 <form onSubmit={(event) => { this.handleOnSubmit(event) }}>
+//                     <label>Your name:</label>
+//                     <input value={this.state.name} type="text" onChange={(event) => { this.handleOnChaneInputName(event) }} />
+//                     <button>Submit</button>
+
+//                     <label>Your age:</label>
+//                     <input value={this.state.age} type="text" onChange={(event) => { this.handleOnChaneInputAge(event) }} />
+//                     <button>Submit</button>
+//                 </form>
+//             </>
+//         )
+//     }
+// }
+
+const AddUserInfor = (props) => {
+    const [name, setName] = useState('');
+    const [address, setAdress] = useState('HCM');
+    const [age, setAge] = useState('');
     // neu khong chuyen ve arrow function se bao loi do js '() => {}'
-    handelClick = (event) => {
+    const handelClick = (event) => {
         console.log(" You click me ")
-        console.log('My name is:', this.state.name)
+        console.log('My name is:', name)
         console.log(event)
-        // cap nhat lai ten va tuoi tu state
-        this.setState({
-            name: 'Le Phu',
-            age: Math.floor((Math.random() * 100) + 1)
-        })
+        setName('Le Phu');
+        setAge(Math.floor((Math.random() * 100) + 1))
 
     }
 
-    handleOnChaneInputName = (event) => {
+    const handleOnChaneInputName = (event) => {
         // cap nhat lai ten tu state
-        this.setState({
-            name: event.target.value
-        })
+        setName(event.target.value)
     }
 
-    handleOnChaneInputAge = (event) => {
+    const handleOnChaneInputAge = (event) => {
         // cap nhat lai tuoi tu state
-        this.setState({
-            age: event.target.value
-        })
+        setAge(event.target.value)
     }
     // lay du lieu tu input va truyen toi lop cha tu props handelAddNewUser
-    handleOnSubmit = (event) => {
+    const handleOnSubmit = (event) => {
         event.preventDefault(); // nhan submit se khong load loai trang
-        this.props.handelAddNewUser({
+        props.handelAddNewUser({
             id: Math.floor((Math.random() * 100) + 1) + ' random',
-            name: this.state.name,
-            age: this.state.age
+            name: name,
+            age: age
         })
     }
-    render() {
-        return (
-            <>
-                My name is {this.state.name} and I'm {this.state.age} years old, I'm from {this.state.address}
-                <button onClick={(event) => { this.handelClick(event) }}>Click me</button>
-                <button onMouseOver={this.handelClick}> Hover click me</button>
-                <form onSubmit={(event) => { this.handleOnSubmit(event) }}>
-                    <label>Your name:</label>
-                    <input value={this.state.name} type="text" onChange={(event) => { this.handleOnChaneInputName(event) }} />
-                    <button>Submit</button>
-
-                    <label>Your age:</label>
-                    <input value={this.state.age} type="text" onChange={(event) => { this.handleOnChaneInputAge(event) }} />
-                    <button>Submit</button>
-                </form>
-            </>
-        )
-    }
+    return (
+        <>
+            My name is {name} and I'm {age} years old, I'm from {address}
+            <button onClick={(event) => { handelClick(event) }}>Click me</button>
+            <button onMouseOver={handelClick}> Hover click me</button>
+            <form onSubmit={(event) => { handleOnSubmit(event) }}>
+                <label>Your name:</label>
+                <input value={name} type="text" onChange={(event) => { handleOnChaneInputName(event) }} />
+                <button>Submit</button>
+                <label>Your age:</label>
+                <input value={age} type="text" onChange={(event) => { handleOnChaneInputAge(event) }} />
+                <button>Submit</button>
+            </form>
+        </>
+    )
 }
-
 export default AddUserInfor;
--- src/components/MyComponents.js
+++ src/components/MyComponents.js
@@ -1,45 +1,74 @@
 // class components
 // function components
 
-import React from "react"; // IMPORT React
+import React, { useState } from "react"; // IMPORT React
 import AddUserInfor from "./AddUserinfo";
 import DisplayInfo from "./DisplayInfor";
 
 // thua ke lai Component cua react
-class MyComponents extends React.Component {
-    state = {
-        listUser: [
+// class MyComponents extends React.Component {
+//     state = {
+//         listUser: [
+//             { id: 1, name: 'Phuoc Hai', age: 17 },
+//             { id: 2, name: 'Le Phu', age: 27 },
+//             { id: 3, name: 'Xuan Hieu', age: 30 },
+//         ]
+//     }
+//     // lay du lieu user tu lop con AddUserInfo -> submit
+//     handelAddNewUser = (obuser) => {
+//         this.setState({
+//             listUser: [obuser, ...this.state.listUser]
+//         })
+//     }
+//     handelDeleteUser = (userId) => {
+//         this.setState({
+//             listUser: [...this.state.listUser.filter(item => item.id !== userId)] // copy lai array va loc lai theo dieu kien
+//         })
+//     }
+//     // JSX  
+//     // props chi truyen duoc strinh con cac thong so khac chi co the truyen thong qua {}
+//     render() {
+//         return (
+//             <div>
+//                 <AddUserInfor handelAddNewUser={this.handelAddNewUser} />
+//                 <br></br>
+//                 <DisplayInfo
+//                     listUser={this.state.listUser}
+//                     handelDeleteUser={this.handelDeleteUser}
+//                 />
+//             </div >
+//         );
+
+//     }
+// }
+
+const MyComponents = (props) => {
+
+    const [listUser, setListUsers] = useState(
+        [
             { id: 1, name: 'Phuoc Hai', age: 17 },
             { id: 2, name: 'Le Phu', age: 27 },
             { id: 3, name: 'Xuan Hieu', age: 30 },
         ]
-    }
+    );
     // lay du lieu user tu lop con AddUserInfo -> submit
-    handelAddNewUser = (obuser) => {
-        this.setState({
-            listUser: [obuser, ...this.state.listUser]
-        })
+    const handelAddNewUser = (obuser) => {
+        setListUsers([obuser, ...listUser])
     }
-    handelDeleteUser = (userId) => {
-        this.setState({
-            listUser: [...this.state.listUser.filter(item => item.id !== userId)] // copy lai array va loc lai theo dieu kien
-        })
+    const handelDeleteUser = (userId) => {
+        setListUsers(
+            [...listUser.filter(item => item.id !== userId)] // copy lai array va loc lai theo dieu kien
+        )
     }
-    // JSX  
-    // props chi truyen duoc strinh con cac thong so khac chi co the truyen thong qua {}
-    render() {
-        return (
-            <div>
-                <AddUserInfor handelAddNewUser={this.handelAddNewUser} />
-                <br></br>
-                <DisplayInfo
-                    listUser={this.state.listUser}
-                    handelDeleteUser={this.handelDeleteUser}
-                />
-            </div >
-        );
-
-    }
+    return (
+        <div>
+            <AddUserInfor handelAddNewUser={handelAddNewUser} />
+            <br></br>
+            <DisplayInfo
+                listUser={listUser}
+                handelDeleteUser={handelDeleteUser}
+            />
+        </div >
+    )
 }
-
 export default MyComponents;
